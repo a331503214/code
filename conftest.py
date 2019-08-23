d7ed1f0789d95e0cb179e08b136a354fe18b0020
liulipeng <331503214@qq.com>
2019-08-23 10:38:46 +0800

    init

---
+++ conftest.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time   :2019-08-22 18:00 
+# @Author : Liulipeng
+# @File   :conftest.py
+
+import sys
+import os
+
+sys.path.append(os.path.dirname('./allure'))
